a552a4d60a71cffc5a221c986ac95bdb5636ab92
Francis Obiagwu <cyber.francis@comcast.net>
2024-01-02 13:54:45 -0600

    fix: initial commit

---
+++ src/slack_notification/singleton_base.py
@@ -0,0 +1,10 @@
+
+class Singleton(object):
+    _instances = {}
+
+    def __new__(cls, *args, **kwargs) -> dict:
+        if cls not in cls._instances:
+            instance = super().__new__(cls)
+            cls._instances[cls] = instance
+        
+        return cls._instances[cls]
